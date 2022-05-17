4b5efd46267b55a7533541020d556ca483faffad
Déreck Lachance <82069370+2043711@users.noreply.github.com>
2022-05-16 23:24:16 -0400

    Update env.php

--- config/env.php
+++ config/env.php
@@ -14,5 +14,13 @@ switch (MODE) {
         $_ENV['database'] = 'serviceweb_examfinal';
         $_ENV['password'] = 'mysql';
         break;
+        
+    case "prod":
+        // Configuration BD pour Heroku
+        $_ENV['host'] = 'us-cdbr-east-05.cleardb.net';
+        $_ENV['username'] = 'bf9a85969e8e77';
+        $_ENV['database'] = 'heroku_772d1f6a72ac3f9';
+        $_ENV['password'] = '1bb801fd';
+        break;
 
 };
